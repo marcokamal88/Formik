4b1fbf4fef60d21118a81d48ed1f622fe239c4ee
marcokamal88 <marcolkamal@gmail.com>
2024-03-18 19:07:24 +0200

    login form using Formik

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Formik, Field, Form } from "formik";
 import { useState } from "react";
-
+import * as Yup from "yup";
+const validation = Yup.object().shape({
+  fname: Yup.string()
+    .min(3, "too short")
+    .max(10, "too long")
+    .required("Required"),
+  lname: Yup.string()
+    .min(3, "too short")
+    .max(10, "too long")
+    .required("Required"),
+  Email: Yup.string().email("invalid email").required("Required"),
+  password: Yup.string().min(8).required("Required"),
+  wife: Yup.string()
+    .min(3, "too short")
+    .max(10, "too long")
+    .required("Required"),
+});
 export default function MyForm() {
-  const [martial, setMartial] = useState("none");
+  const [martial, setMartial] = useState(null);
   const handleMartial = (e) => {
     setMartial(e.target.value);
   };
@@ -10,60 +27,83 @@ export default function MyForm() {
     <>
       <Formik
         initialValues={{
-          fname: "first name",
-          lname: "last name",
-          Email: "Email",
-          password: "password",
-          wife: "wifes name",
+          fname: "",
+          lname: "",
+          Email: "",
+          password: "",
+          wife: "",
         }}
+        validationSchema={validation}
         onSubmit={async (values) => {
           await new Promise((resolve) => setTimeout(resolve, 500));
           alert(JSON.stringify(values, null, 2));
         }}
       >
-        <Form className="form">
-          <div>
-            <label>First name:</label>
-            <Field className="fields" type="text" id="fname" name="fname" />
-          </div>
-          <div>
-            <label>Last name:</label>
-            <Field className="fields" type="text" id="lname" name="lname" />
-          </div>
-          <div>
-            <label>Email:</label>
-            <Field className="fields" type="text" id="Email" name="Email" />
-          </div>
-          <div>
-            <label>password:</label>
-            <Field
-              className="fields"
-              type="password"
-              id="password"
-              name="password"
-            />
-          </div>
-          <div>
-            <label>marital Status: </label>
-            <select
-              className="fields"
-              id="maritalStatus"
-              value={martial}
-              onChange={handleMartial}
-            >
-              <option>Select marital Status</option>
-              <option value="single">Single</option>
-              <option value="married">Married</option>
-            </select>
-            {martial === "married" && (
-              <div>
-                <label >Wife's Name:</label>
-                <Field name="wife" className="fields" type="text" id="wifeName" />
-              </div>
-            )}
-          </div>
-          <button type="submit">Submit</button>
-        </Form>
+        {({ errors, touched }) => (
+          <Form className="form">
+            <div>
+              <label>First name:</label>
+              <Field className="fields" type="text" id="fname" name="fname" />
+              {errors.fname && touched.fname ? (
+                <div className="errors">*{errors.fname}</div>
+              ) : null}
+            </div>
+            <div>
+              <label>Last name:</label>
+              <Field className="fields" type="text" id="lname" name="lname" />
+              {errors.lname && touched.lname ? (
+                <div className="errors">*{errors.lname}</div>
+              ) : null}
+            </div>
+            <div>
+              <label>Email:</label>
+              <Field className="fields" type="text" id="Email" name="Email" />
+              {errors.Email && touched.Email ? (
+                <div className="errors">*{errors.Email}</div>
+              ) : null}
+            </div>
+            <div>
+              <label>password:</label>
+              <Field
+                className="fields"
+                type="password"
+                id="password"
+                name="password"
+              />
+              {errors.password && touched.password ? (
+                <div className="errors">* {errors.password}</div>
+              ) : null}
+            </div>
+            <div>
+              <label>marital Status: </label>
+              <select
+                className="fields"
+                id="maritalStatus"
+                value={martial}
+                onChange={handleMartial}
+              >
+                <option>Select marital Status</option>
+                <option value="single">Single</option>
+                <option value="married">Married</option>
+              </select>
+              {martial === "married" && (
+                <div>
+                  <label>Wife's Name:</label>
+                  <Field
+                    name="wife"
+                    className="fields"
+                    type="text"
+                    id="wifeName"
+                  />
+                  {errors.wife && touched.wife ? (
+                    <div className="errors">*{errors.wife}</div>
+                  ) : null}
+                </div>
+              )}
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
       </Formik>
     </>
   );
